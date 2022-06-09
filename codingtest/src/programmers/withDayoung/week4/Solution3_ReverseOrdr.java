2ac7adc1999f155d2e56b37c9544060ae16c78ab
HyangKeun Choi <gidrms99@naver.com>
2022-06-09 22:11:50 +0900

    feat : 4주차 3번문제 풀이

---
+++ codingtest/src/programmers/withDayoung/week4/Solution3_ReverseOrdr.java
@@ -0,0 +1,31 @@
+package programmers.withDayoung.week4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/*
+ * 정수 내림차순으로 배치하기
+ * 역순 정렬하기
+ */
+public class Solution3_ReverseOrdr {
+    public static void main(String[] args) {
+        Solution3_ReverseOrdr solution3 = new Solution3_ReverseOrdr();
+        System.out.println(solution3.solution(8134512));
+    }
+    public long solution(long n) {
+        // long to string
+        String s = String.valueOf(n);
+
+        // string to string Array
+        String[] strArr = s.split("");
+
+        strArr = Arrays.stream(strArr).sorted(Comparator.reverseOrder())
+                    .collect(Collectors.toList()).toArray(String[]::new);
+
+        // string array to string
+        String str = String.join(",", strArr);
+
+        return Long.parseLong(str.replaceAll(",", ""));
+    }
+}
