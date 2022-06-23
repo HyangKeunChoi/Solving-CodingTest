ef1995497e7826269078eed154566c8f1e1ee8f4
HyangKeun Choi <gidrms99@naver.com>
2022-06-23 23:11:27 +0900

    feat : 스택 한문제 추가

---
+++ codingtest/src/baekjoon/step/stack/Solution3_Zero.java
@@ -0,0 +1,40 @@
+package baekjoon.step.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution3_Zero {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < input; i++) {
+            int newInput = scanner.nextInt();
+
+            if(i == 0 && newInput == 0) {
+
+            } if(newInput == 0) {
+                stack.pop();
+            } else {
+                stack.push(newInput);
+            }
+        }
+
+        int sum = 0;
+
+        int size = stack.size(); // pop하면 사이즈가 바뀜
+
+        for (int i = 0; i < size; i++) {
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+        scanner.close();
+    }
+
+
+}
